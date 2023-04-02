ec1fea5f292ad4f69e2190b37f10b381b96540f0
Pietrorame <126229386+Pietrorame@users.noreply.github.com>
2023-04-02 22:19:17 +0200

    es 1

--- EDUCAZIONE_CIVICA/EDUCAZIONE_CIVICA/Program.cs
+++ EDUCAZIONE_CIVICA/EDUCAZIONE_CIVICA/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public static class Applicazione
+{
+    private static string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+    private static int contatore = 0;
+    public static string Es1(string s, int n)
+    {
+        int index = 0;
+        string stringaFinale = "";
+        foreach (var item in s)
+        {
+            s = s.ToLower();
+            index = 0;
+            while (item != alfabeto[index])
+            {
+                index++;
+            }
+            index += n;
+            if (index > 25)
+            {
+                index -= 25;
+            }
+            stringaFinale += alfabeto[index];
+        }
+        return stringaFinale; }
+    
+
+    
