00cfff12281a1a3246db75b2ed6d2c686f50ef31
Pietrorame <126229386+Pietrorame@users.noreply.github.com>
2023-04-02 22:20:11 +0200

    es2

--- EDUCAZIONE_CIVICA/EDUCAZIONE_CIVICA/Program.cs
+++ EDUCAZIONE_CIVICA/EDUCAZIONE_CIVICA/Program.cs
@@ -23,7 +23,60 @@ public static class Applicazione
             }
             stringaFinale += alfabeto[index];
         }
-        return stringaFinale; }
+        return stringaFinale;
+
+        using System;
+
+public static class Applicazione
+    {
+        private static string alfabeto = "abcdefghijklmnopqrstuvwxyz";
+        private static int contatore = 0;
+        public static string Es1(string s, int n)
+        {
+            int index = 0;
+            string stringaFinale = "";
+            foreach (var item in s)
+            {
+                s = s.ToLower();
+                index = 0;
+                while (item != alfabeto[index])
+                {
+                    index++;
+                }
+                index += n;
+                if (index > 25)
+                {
+                    index -= 25;
+                }
+                stringaFinale += alfabeto[index];
+            }
+            return stringaFinale;
+        }
+
+        public static string Es2(string s, int n)
+        {
+            int index = 0;
+            string stringaFinale = "";
+            foreach (var item in s)
+            {
+                s = s.ToLower();
+                index = 0;
+                while (item != alfabeto[index])
+                {
+                    index++;
+                }
+                index -= n;
+                if (index < 0)
+                {
+                    index += 25;
+                }
+                stringaFinale += alfabeto[index];
+            }
+            return stringaFinale;
+        }
+
+       
+        }
     
 
     
